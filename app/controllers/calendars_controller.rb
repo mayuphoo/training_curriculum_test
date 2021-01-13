b79daba0869a3885eda2d6820b1d37f1e4172bc1
mayuphoo <76111639+mayuphoo@users.noreply.github.com>
2021-01-13 22:00:34 +0900

    issure6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,21 +20,29 @@ class CalendarsController < ApplicationController
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+  
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
+    
     @week_days = []
 
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
+    plans = Plan.where(date: @todays_date..@todays_date + 6) 
+    # 今日〜6日後までのデータ。
 
     7.times do |x|
-      today_plans = []
-      plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      today_plans = [] #空の配列を用意
+      plans.each do |plan| #今日〜6日後までのデータを、planとして1つずつ取り出す。
+        today_plans.push(plan.plan) if plan.date == @todays_date + x #1日分の予定を、todays_plan配列に追加する。もし日付が7日後までの間だったら。
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+    wday_num = Date.today.wday + x
+      if wday_num >= 7
+        wday_num = wday_num -7
+        # 7以上の数字になったら−７して0に戻す。
+      end
+
+      days = { :month => (@todays_date + x).month, :date => @todays_date.day + x, :plans => today_plans, :wday => wdays[wday_num] }
       @week_days.push(days)
     end
 
